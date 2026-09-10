16a6d23701a513a8d84b6511ee9d0324ffa1225b
Yi Ding <ding_yi0731@tju.edu.cn>
2026-09-10 14:41:49 -0400

    Update homepage CV with polished publication and research details

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ const fallbackConfig = {
             title: 'Twitter'
         },
         cv: {
-            url: 'pdf/CV.pdf',
+            url: 'pdf/CV.pdf?v=20260910-v2',
             icon: 'fa-solid fa-file-lines',
             title: 'CV'
         },
